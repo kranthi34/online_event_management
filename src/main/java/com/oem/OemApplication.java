a6d9f06c7b922a7071014533bc54223dc281090e
Kranthi Kumar <kranthi34@users.noreply.github.com>
2018-05-21 00:08:17 +0530

    added files

---
+++ src/main/java/com/oem/OemApplication.java
@@ -0,0 +1,12 @@
+package com.oem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/oem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.oem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OemApplication.class);
+	}
+
+}
